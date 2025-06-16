73023ab1cbb9b94c39b0fe80f565eb75cd331402
sherifbai <ruzmetov_sh@tuneconsulting.net>
2025-06-16 15:50:34 +0500

    delete guard

--- apps/dmr-server/src/modules/auth/auth.module.ts
+++ apps/dmr-server/src/modules/auth/auth.module.ts
@@ -2,11 +2,10 @@ import { Module } from '@nestjs/common';
 import { CentOpsModule } from '../centops/centops.module';
 import { JwtModule } from '@nestjs/jwt';
 import { AuthService } from './auth.service';
-import { WsAuthGuard } from './guards/ws-auth.guard';
 
 @Module({
   imports: [CentOpsModule, JwtModule.register({ verifyOptions: { algorithms: ['RS256'] } })],
-  providers: [AuthService, WsAuthGuard],
-  exports: [AuthService, WsAuthGuard],
+  providers: [AuthService],
+  exports: [AuthService],
 })
 export class AuthModule {}
