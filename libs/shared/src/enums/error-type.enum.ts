184e8458a03b64950ef7590a211f9266fbe3e26e
sherifbai <ruzmetov_sh@tuneconsulting.net>
2025-06-19 14:47:20 +0500

    added types & interfaces

---
+++ libs/shared/src/enums/error-type.enum.ts
@@ -0,0 +1,4 @@
+export enum ErrorType {
+  DECRYPTION_FAILED = 'dmr:agents:decryption-failed',
+  SIGNATURE_VALIDATION_FAILED = 'dmr:agents:signature-validation-failed',
+}
--- libs/shared/src/interfaces/index.ts
+++ libs/shared/src/interfaces/index.ts
@@ -1,3 +1,4 @@
 export * from './centops.interface';
 export * from './jwt-payload.interfaces';
 export * from './agent.interface';
+export * from './dmr-error.interface';
