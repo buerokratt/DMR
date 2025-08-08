448e047897271edacc10a67c2ae67481ae8cbd6d
Bobur Nazirov <86878919+aNazirov@users.noreply.github.com>
2025-08-08 13:31:45 +0500

    14: CentOps connection and auth (#219)

--- apps/dmr-server/src/common/config/cent-ops.config.ts
+++ apps/dmr-server/src/common/config/cent-ops.config.ts
@@ -7,11 +7,15 @@ export const CENT_OPS_CONFIG_TOKEN = Symbol('CENT_OPS_CONFIG_TOKEN');
 const variables = Utils.validateObject(
   {
     url: String(process.env.CENTOPS_CONFIGURATION_URL),
+    apiKey: String(process.env.CENTOPS_CONFIGURATION_API_KEY),
     cronTime: String(process.env.CENTOPS_CONFIGURATION_CRON_TIME),
+    apiSecret: String(process.env.CENTOPS_CONFIGURATION_API_SECRET),
   },
   {
     url: Joi.string().uri().required(),
+    apiKey: Joi.string().required(),
     cronTime: Joi.string().default('*/30 * * * *'),
+    apiSecret: Joi.string().required(),
   },
 );
 
